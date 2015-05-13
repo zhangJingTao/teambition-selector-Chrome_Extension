1e6d2651bc2f9f3435cfcdd62c54a6b8282248a8
张京涛 <zhangjingtaosleep@163.com>
2015-05-13 09:57:39 +0800

    调整代码格式

--- content.js
+++ content.js
@@ -1,16 +1,16 @@
-chrome.runtime.onMessage.addListener(  function(request, sender, sendResponse) {
-	  if (request.cmd== "getDataId"){
-		  var lis = document.getElementsByClassName("scrum-stage");
-		  var ids = new Array();
-		  for(var i=0;i<lis.length;i++){
-			  ids.push(lis[i].getAttribute("data-id"));
-		  }		  
-		  sendResponse(
-		  {
-			  kws: ids,
-			  count: document.getElementsByClassName("scrum-stage").length,
-			  lock_count:document.getElementsByClassName("locked-stage").length,
-			  uid:window.location.href.split("\/")[4]
-		  }); 
-	  }
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.cmd == "getDataId") {
+        var lis = document.getElementsByClassName("scrum-stage");
+        var ids = new Array();
+        for (var i = 0; i < lis.length; i++) {
+            ids.push(lis[i].getAttribute("data-id"));
+        }
+        sendResponse(
+            {
+                kws: ids,
+                count: document.getElementsByClassName("scrum-stage").length,
+                lock_count: document.getElementsByClassName("locked-stage").length,
+                uid: window.location.href.split("\/")[4]
+            });
+    }
 });
